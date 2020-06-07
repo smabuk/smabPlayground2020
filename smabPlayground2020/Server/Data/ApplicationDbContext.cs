84230da73541f91e5e0318ad18c821c67e8fa747
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2020-06-07 13:16:01 +0100

    Add project files.

---
+++ smabPlayground2020/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using smabPlayground2020.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace smabPlayground2020.Server.Data
+{
+	public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(
+			DbContextOptions options,
+			IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+		{
+		}
+	}
+}
