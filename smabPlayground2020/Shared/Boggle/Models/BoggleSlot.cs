3bec508fa0ffe16f002b88704f268c2881b63e02
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2020-11-17 20:16:27 +0000

    Boggle WIP
    Move from Table to Grid
    Change centering mechanism
    Introduce slots instead of dice as the primary display unit

---
+++ smabPlayground2020/Shared/Boggle/Models/BoggleSlot.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Smab.DiceAndTiles;
+
+namespace Smab.Boggle.Models
+{
+    public class BoggleSlot
+    {
+		public int Id { get; init; }
+
+        public int X { get; init; }
+        public int Y { get; init; }
+        public LetterDie Die { get; set; }
+
+        public bool IsMine { get; set; }
+        public int AdjacentMines { get; set; }
+        public bool IsRevealed { get; set; }
+        public bool IsFlagged { get; set; }
+
+        public BoggleSlot(int id, int x, int y)
+        {
+            Id = id;
+            X = x;
+            Y = y;
+        }
+        public BoggleSlot(int id, int x, int y, LetterDie die)
+        {
+            Id = id;
+            X = x;
+            Y = y;
+            Die = die;
+        }
+
+        public void Flag()
+        {
+            if (!IsRevealed)
+            {
+                IsFlagged = !IsFlagged;
+            }
+        }
+
+        public void Reveal()
+        {
+            IsRevealed = true;
+            IsFlagged = false; //Revealed panels cannot be flagged
+        }
+    }
+}
---
+++ smabPlayground2020/Shared/Boggle/Models/BoggleTray.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Smab.DiceAndTiles;
+
+namespace Smab.Boggle.Models
+{
+    public partial class BoggleTray
+    {
+		public BoggleDice BoggleSet { get; set; }
+        public BoggleDice.BoggleType BoggleSetType { get; set; } = BoggleDice.BoggleType.Classic4x4;
+		public List<BoggleSlot> Slots { get; set; } = new();
+
+        public int Width => BoggleSet.BoardSize;
+        public int Height => BoggleSet.BoardSize;
+        public int DiceCount => BoggleSet.NoOfDice;
+        // public GameStatus Status { get; set; }
+
+        public Stopwatch Stopwatch { get; set; }
+
+		public BoggleTray()
+		{
+            Reset();
+		}
+
+		public BoggleTray(BoggleDice.BoggleType boggleSetType)
+		{
+			BoggleSetType = boggleSetType;
+            Reset();
+		}
+
+        public void Reset()
+		{
+            StartNewGame(BoggleSetType);
+			// Stopwatch = new();
+		}
+
+		private void StartNewGame(BoggleDice.BoggleType boggleSetType)
+		{
+			BoggleSet = new BoggleDice(BoggleSetType);
+			BoggleSet.ShakeAndFillBoard();
+            int id = 1;
+			int setIndex = 0;
+			for (int i = 1; i <= Height; i++)
+			{
+				for (int j = 1; j <= Width; j++)
+				{
+					Slots.Add(new BoggleSlot(id++, j, i, BoggleSet.Board[setIndex++]));
+				}
+			}
+        }
+
+		public List<BoggleSlot> GetAdjacentSlots(int x, int y)
+		{
+			IEnumerable<BoggleSlot> adjacentSlots = 
+				Slots.Where(slot => slot.X >= (x - 1) && slot.X <= (x + 1) && slot.Y >= (y - 1) && slot.Y <= (y + 1));
+			IEnumerable<BoggleSlot> currentSlot = Slots.Where(slot => slot.X == x && slot.Y == y);
+			return adjacentSlots.Except(currentSlot).ToList();
+		}
+	}
+}
