8924146aad1423c570a60c3560817ab17d6e82b3
smabuk <2011834+smabuk@users.noreply.github.com>
2021-10-20 15:29:04 +0100

    CustomIcon

---
+++ smabPlayground2020/Shared/Components/IconEnums.cs
@@ -0,0 +1,39 @@
+﻿namespace smabPlayground2020.Shared.Components;
+
+public enum IconType {
+	FontAwesome5Solid = 1,
+	FontAwesome5Regular,
+	FontAwesome5Light,
+	FontAwesome5DuoTone,
+	FontAwesome5Thin,
+	FontAwesome5Brands,
+	FaSolid = 101,
+	FaRegular,
+	FaLight,
+	FaDuoTone,
+	FaThin,
+	FaBrands,
+	FontAwesomeSolid,
+	FontAwesomeRegular,
+	FontAwesomeLight,
+	FontAwesomeDuoTone,
+	FontAwesomeThin,
+	FontAwesomeBrands,
+	OpenIcon = 201,
+}
+
+//public enum FontAwesomeType {
+//	Solid,
+//	Regular,
+//	Light,
+//	DuoTone,
+//	Thin,
+//	Brands
+//}
+
+public enum CustomIconPosition {
+	TopLeft,
+	TopRight,
+	BottomLeft,
+	BottomRight
+}
