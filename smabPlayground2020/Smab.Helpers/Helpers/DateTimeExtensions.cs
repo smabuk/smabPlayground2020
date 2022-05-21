d055469a860b8b7d7fa3e8a803493760635309b5
smabuk <2011834+smabuk@users.noreply.github.com>
2022-05-21 11:31:50 +0100

    Move Smab.Helpers to own external NuGet package

--- smabPlayground2020/Smab.Helpers/Helpers/DateTimeExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Smab.Helpers;
-
-public static class DateTimeExtensions
-{
-    internal static readonly string DD_MMMM_YYYY = "dd MMMM, yyyy";
-
-	public static string ToDateLongMonthYearString(this DateTime dateTime) => dateTime.ToString(DD_MMMM_YYYY);
-}
